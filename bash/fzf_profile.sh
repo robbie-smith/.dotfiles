176a25d33ba3c67a1c4a3e672b935377fd9e7c6b
Robbie Smith <robert.smith160@icloud.com>
2017-02-26 11:34:04 -0700

    Updates the git delete branch function

--- bash/fzf_profile.sh
+++ bash/fzf_profile.sh
@@ -104,11 +104,25 @@ gcb() {
 }
 
 gdb() {
-  local branches branch
-  branches=$(git branch --merged) &&
-  branch=$(echo "$branches" | fzf +m) &&
-    git branch -d $(echo "$branch" | sed "s/.* //") && gdb
+  local tags branches target
+    tags=$(
+        git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}') || return
+    branches=$(
+        git branch --color | grep -v HEAD             |
+        sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
+        sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
+    target=$(
+        (echo "$tags"; echo "$branches") |
+        fzf-tmux -l40 -- --no-hscroll --ansi +m -d "\t" -n 2 -1 -q "$*") || return
+    git branch -D $(echo "$target" | awk '{print $2}')
 }
+
+# gdb() {
+#   local branches branch
+#   branches=$(git branch --merged) &&
+#   branch=$(echo "$branches" | fzf +m) &&
+#     git branch -d $(echo "$branch" | sed "s/.* //") && gdb
+# }
 
 # show commit history, enter to select commit and  ctrl-d to see the diff
 gshow() {
