392871e822687b9c132762cf4110a0228f8de0df
Robbie Smith <robbie.smith@procore.com>
2017-12-12 16:33:57 -0800

    Updates aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -17,20 +17,19 @@ alias gc="git checkout"
 alias gmaster="git checkout master"
 alias gac="git add . && git commit -m"
 alias gpom="git pull origin master"
+alias pull="git pull"
 alias home="cd ~"
 alias hidedotfiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 alias login_dev_db='pgcli procore_lite1 -h 192.168.41.240 -U procore_db'
 alias ls="ls -GFh"
-alias pr="hub pull-request -l \"Needs Code Review\", \"Needs Testing\", \"#squad-insights\" -o "
 alias procore="cd ~/Dev/procore/"
-alias makepr="hub pull-request -o -F ~/.dotfiles/PULL_REQUEST_TEMPLATE.md | pbcopy -l \"Needs Code Review\", \"Needs Testing\", \"#squad-insights\" "
 alias notes="cd ~/dev/work_notes/ && nvim"
 alias show='ls'
 alias showdotfiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias reload='source ~/.bash_profile'
 alias runexpress="DEBUG=myapp:* npm start --scripts-prepend-node-path"
 alias run_hydra="cd ~/Dev/procore/hydra_clients/insights && yarn start"
-alias run_insights="cd ~/Dev/procore && DEV_MODE=insights bin/rails s"
+alias run_insights="cd ~/Dev/procore && spring stop && DEV_MODE=insights bin/rails s"
 alias run_hydra_proc="DEV_MODE=insights foreman start -f Procfile.hydra"
 alias run_wrench="cd ~/Dev/procore/wrench/ && HOST=localhost PORT=8080 npm run dev"
 alias vim='nvim'
