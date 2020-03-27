ef9cb1118a234e4b546e4a126efd49e9d7cc078d
Robbie Smith <robert.smith160@gmail.com>
2020-03-27 14:36:55 -0600

    Removes unsued functions

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -25,41 +25,9 @@ function compare() {
   hub compare `git rev-parse --abbrev-ref HEAD`
 }
 
-function lazyrails() {
-  rails new "$1" -d postgresql --skip-turbolinks --skip-spring -T
-  cd "$1"
-  echo "
-  group :test do
-  gem 'rack_session_access'
-  gem 'capybara'
-  gem 'launchy'
-  gem 'shoulda-matchers'
- end
-  group :development, :test do
-    gem 'rspec-rails'
-    gem 'database_cleaner'
-    gem 'byebug', platform: :mri
-    gem 'factory_girl_rails'
-    gem 'pry'
-    gem 'pry-byebug'
-  end " >> Gemfile
-  bundle install
-  rails g rspec:install
-}
 
 function md () { mkdir -p "$@" && cd "$@";}
 
-function codi() {
-  local syntax="${1:-python}"
-  shift
-  vim -c \
-    "let g:startify_disable_at_vimenter = 1 |\
-    set bt=nofile ls=0 noru nonu nornu |\
-    hi ColorColumn ctermbg=NONE |\
-    hi VertSplit ctermbg=NONE |\
-    hi NonText ctermfg=0 |\
-    Codi $syntax" "$@"
-}
 export PATH="/usr/local/opt/postgresql@9.5/bin:$PATH"
 
 # added by Anaconda3 5.0.1 installer
