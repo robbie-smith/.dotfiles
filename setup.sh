a09f296ac7780c9a2d9ee828caaee9a420ff7ab0
Robbie Smith <robert.smith160@gmail.com>
2020-03-28 09:45:13 -0600

    Refactors into functions

--- setup.sh
+++ setup.sh
@@ -1,35 +1,56 @@
 #! /bin/sh
-if [[ ! -d $HOME/.config ]]; then
-  echo "Creating config directory in $HOME/.config/"
-  mkdir $HOME/.config
-fi
-
-if [[ ! -d $HOME/.config/nvim/plugged ]]; then
-  echo "Installing vim plug"
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-
-
-for f in $HOME/.dotfiles/dotfiles/*
-do
-  if [[ -f $f ]]; then
-    ln -s $f $HOME/.$(basename $f)  #statements
+create_config_directory(){
+  if [[ ! -d $HOME/.config ]]; then
+    echo "Creating config directory in $HOME/.config/ ."
+    mkdir $HOME/.config
   fi
-done
+}
 
-if [[ -f $HOME/.dotfiles/bash/bash_profile.sh ]]; then
-  ln -s $HOME/.dotfiles/bash/bash_profile.sh $HOME/.bash_profile
-fi
+install_vim_plug(){
+  if [[ ! -d $HOME/.config/nvim/plugged ]]; then
+    echo "Installing vim plug."
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  fi
+}
 
-if [[ -f /usr/local/Cellar/neovim/0.2.0_1/share/nvim/runtime/colors ]]; then
-  rm -rf /usr/local/Cellar/neovim/0.2.0_1/share/nvim/runtime/colors
-fi
+symlink_dotfiles_dir() {
+  echo "Symlinking dotfiles."
+  for f in $HOME/.dotfiles/dotfiles/*
+  do
+    local parent_file=$f
+    local dotfile=$HOME/.$(basename $f)
+    if [[ -e $parent_file ]]; then
+      ln -s $parent_file $dotfile
+    fi
+  done
+}
 
-if [[ -d $HOME/.dotfiles/nvim ]]; then
-  ln -s $HOME/.dotfiles/nvim ~/.config/
-fi
+symlink_bash_profile(){
+  local bash_profile=$HOME/.dotfiles/bash/bash_profile.sh
+  if [[ -e $bash_profile ]]; then
+    echo "Symlinking bash_profile."
+    ln -s $bash_profile $HOME/.bash_profile
+  fi
+}
+
+remove_neovim_default_colors(){
+  local neovim_colors=/usr/local/Cellar/neovim/0.2.0_1/share/nvim/runtime/colors
+  if [[ -d $neovim_colors  ]]; then
+    echo "Removing default neovim colors."
+    rm -rf $neovim_colors
+  fi
+}
+
+
+symlink_neovim_to_config_directory(){
+  local nvim_directory=$HOME/.dotfiles/nvim
+  if [[ -d $nvim_directory  ]]; then
+    echo "Symlinking neovim directory to config directory."
+    ln -s $nvim_directory ~/.config/
+  fi
+}
+
 
 install_powerline_fonts() {
   git clone https://github.com/powerline/fonts.git --depth=1
@@ -40,6 +61,12 @@ install_powerline_fonts() {
   cd .. && rm -rf fonts
 }
 
+create_config_directory
+install_vim_plug
+symlink_dotfiles_dir
+symlink_bash_profile
+remove_neovim_default_colors
+symlink_neovim_to_config_directory
 install_powerline_fonts
 
 echo "Don't half ass two things, whole ass one thing. - R. Swanson"
